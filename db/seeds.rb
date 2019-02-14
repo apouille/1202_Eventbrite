d38446d16ab212d030a3bfa06e293d80a43631e3
apouille <nguon.melanie@gmail.com>
2019-02-14 15:01:19 +0100

    test heroku 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 require 'faker'
 
 
-# User.destroy_all
+User.destroy_all
 Event.destroy_all
 Attendance.destroy_all
 
-# 3.times do
-# User.create!(
-#   first_name: Faker::Name.first_name,
-#   last_name: Faker::Name.last_name,
-# 	description: Faker::Friends.quote, 
-# 	email: "#{Faker::Name.last_name}@yopmail.com",
-# 	password: "coucoucou"
-# 	)
-# end
+3.times do
+User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+	description: Faker::Friends.quote, 
+	email: "#{Faker::Name.last_name}@yopmail.com",
+	password: "coucoucou"
+	)
+end
 
 3.times do
 Event.create!(
-  title: "#{Faker::OnePiece.character}",
+  title: Faker::Lorem.words,
 	description: "#{Faker::OnePiece.quote}", 
 	start_date: Faker::Date.forward(30),
 	duration: 5 * rand(1..120),
